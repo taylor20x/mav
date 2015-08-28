3045d0bc2dec0c1eb6aa3e1b465c59929fbfb9a6
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-08-28 10:49:21 -0500

    Fix: Clarify and correct problem description. Add: More tests, per in-class discussion.

--- homework/hw1.py
+++ homework/hw1.py
@@ -12,14 +12,20 @@
 def pick_val(
   # The sequence to pick from.
   seq,
-  # The element to select. Defaults to 4th from the end. 
   #
-  # * If the sequence is shorter than 4 elements, select the third, second, first, or raise an IndexError exception if the sequence is empty.
-  # * If the element is positive, index from the beginning of the list: element == -2 returns seq[2]. Again, if the list is less than element in length, return an earlier value.
-  element):
+  # The index of element to select, measured from the end of the list. That is,
+  # 1 referes to the last item in seq, 2 to the second item from the end, etc.
+  # This parameter defaults to the 4th from the end.
+  #
+  # * If the sequence is shorter than 4 elements, select the third, second,
+  #   first, or raise an IndexError exception if the sequence is empty.
+  # * If the index is positive, select from the beginning of the list:
+  #   index == -2 returns seq[2]. Again, if the list is less than index in
+  #   length, return an earlier value.
+  index=4):
 
     # Dummy code -- replace this with your code.
-    raise IndexError
+    raise TypeError
 
 # Tests
 # =====
@@ -45,18 +51,28 @@ class TestPickVal(object):
             assert False
         except IndexError:
             pass
-    
+
     # Test passing a non-integral type for the index.
     def test_5(self):
         try:
             pick_val(range(10), 1.5)
             assert False
-        except IndexError:
+        except TypeError:
             pass
-        
+
     # Test picking values outside the range.
     def test_6(self):
         assert pick_val(range(10), 11) == 0
+
+    # Test picking values from other types (string, tuple).
+    def test_7(self):
+        assert pick_val('hello') == 'e'
+        assert pick_val('hello', 10) == 'h'
+        assert pick_val('hello', 1) == 'o'
+
+        assert pick_val(tuple(range(10))) == 6
+        assert pick_val(tuple(range(10), 3)) == 7
+        assert pick_val(tuple(range(10), 11)) == 0
 
 # Test driver code
 # ================
@@ -67,6 +83,8 @@ def run_tests():
     tpv.test_3()
     tpv.test_4()
     tpv.test_5()
+    tpv.test_6()
+    tpv.test_7()
     print('All tests passed.')
 
 if __name__ == '__main__':
